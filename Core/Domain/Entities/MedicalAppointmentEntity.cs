fa0d817d4a84714af8dec4e966b4630786094293
Andrés A. Cortés <andresbeto315@gmail.com>
2023-08-06 10:30:42 -0500

    Cambiar tipo de dato

--- Core/Domain/Entities/MedicalAppointmentEntity.cs
+++ Core/Domain/Entities/MedicalAppointmentEntity.cs
@@ -7,7 +7,7 @@ namespace Domain.Entities
         public long Id { get; set; }
         public int UserDoctorId { get; set; }
         public int UserPatientId { get; set; }
-        public DateOnly Date { get; set; }
+        public DateTime Date { get; set; }
         public short StateId { get; set; }
         public string Observations { get; set; }
     }
